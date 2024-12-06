1fd344c853d5f49ffb81b880eae5177465d90e31
DrRigB <rigdon.windsor@gmail.com>
2024-12-06 08:26:08 -0500

    Add updated Health Hive Django project

--- integration-test/home.js
+++
@@ -1,5 +0,0 @@
-// Get the username from localStorage
-const username = localStorage.getItem('username');
-
-// Display the username in the home page
-document.getElementById('profile-name').textContent = username;
