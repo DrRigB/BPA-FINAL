bc7e648fb0481ec35416bb843936d76569d4033f
nxtcarson <nxtcarson@gmail.com>
2024-12-12 08:00:09 -0500

    save local changes for merge

--- djangotest/accounts/models.py
+++ djangotest/accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    # Add any additional fields you want here
+    phone_number = models.CharField(max_length=15, blank=True)
+    
+    def __str__(self):
+        return self.username
