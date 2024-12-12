17b0df9bb4808347187397b75ff74ed7a71486de
nxtcarson <nxtcarson@gmail.com>
2024-12-12 09:16:22 -0500

    view alterations

--- djangotest/accounts/models.py
+++ djangotest/accounts/models.py
@@ -2,8 +2,23 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    # Add any additional fields you want here
     phone_number = models.CharField(max_length=15, blank=True)
     
+    # Add related_name to avoid clashes
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_set',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        verbose_name='groups',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_set',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions',
+    )
+
     def __str__(self):
         return self.username
