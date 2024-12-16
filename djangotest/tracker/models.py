f7f00633833f7e38d20de64a58560927c0dc9e04
DrRigB <rigdon.windsor@gmail.com>
2024-12-16 08:30:57 -0500

    added homebutton

--- djangotest/tracker/models.py
+++ djangotest/tracker/models.py
@@ -1,5 +1,5 @@
-# tracker/models.py
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 class Activity(models.Model):
     name = models.CharField(max_length=255, default="Exercise")
@@ -11,10 +11,10 @@ class Activity(models.Model):
 
     def calculate_calories_burned(self):
         # Simple formula for calories burned: calories = duration * heart_rate * factor
-        # You can refine this formula based on actual data
         factor = 0.063  # A general factor for calorie estimation
         self.calories_burned = int(self.duration_minutes * self.heart_rate * factor)
         self.save()
 
     def __str__(self):
         return f"{self.name} - {self.date}"
+
