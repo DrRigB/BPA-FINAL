d2a53290766a337a6b1a59a18b2e740ea2b1c7a1
DrRigB <rigdon.windsor@gmail.com>
2025-01-13 09:14:40 -0500

    it might work

--- djangotest/settings.py
+++ djangotest/settings.py
@@ -6,5 +6,13 @@ DATABASES = {
         'PASSWORD': 'your_mysql_password',
         'HOST': 'localhost',
         'PORT': '3306',
+    },
+    'secondary': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'your_secondary_database_name',
+        'USER': 'Carson_Pugh',
+        'PASSWORD': '12345',
+        'HOST': '192.168.56.1',  # Use the correct IP address here
+        'PORT': '3306',
     }
-} 
+}
