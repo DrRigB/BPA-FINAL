f0210c1e002928d8324357099dfb6526522a7c15
DrRigB <rigdon.windsor@gmail.com>
2025-01-14 22:54:54 -0500

    Simplify static files configuration

--- djangotest/mysite/settings.py
+++ djangotest/mysite/settings.py
@@ -125,14 +125,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'djangotest' / 'static',
 ]
 
-# Use WhiteNoise for static files in production
-if not DEBUG:
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-else:
-    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+# Use WhiteNoise for static files
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'
