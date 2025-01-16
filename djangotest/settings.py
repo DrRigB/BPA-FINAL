d34840f354cabde1accde5cb1deaf540c0f4ec1a
DrRigB <rigdon.windsor@gmail.com>
2025-01-16 01:25:09 -0500

    Updated email settings to use environment variables

--- djangotest/settings.py
+++ djangotest/settings.py
@@ -11,11 +11,11 @@ DATABASES = {
 
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True').lower() == 'true'
 EMAIL_USE_SSL = False  # Make sure SSL is off since we're using TLS
-EMAIL_HOST_USER = 'djangohive75@gmail.com'
-EMAIL_HOST_PASSWORD = 'bvga dznf nvwk uqew'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'djangohive75@gmail.com')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'bvga dznf nvwk uqew')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 EMAIL_TIMEOUT = 30  # Add timeout setting
