f631153225bb7069e758c670f4dc8374ae632759
DrRigB <rigdon.windsor@gmail.com>
2025-01-16 01:01:02 -0500

    Updated email settings with new Gmail credentials

--- djangotest/settings.py
+++ djangotest/settings.py
@@ -8,3 +8,12 @@ DATABASES = {
         'PORT': '3306',  # Default MySQL port
     }
 }
+
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'djangohive75@gmail.com'
+EMAIL_HOST_PASSWORD = 'bvga dznf nvwk uqew'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
