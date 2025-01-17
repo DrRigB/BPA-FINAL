f0d5861612577610e5360d9e796fc94c97939640
Rigdon Windsor <rigdon.windsor@gmail.com>
2025-01-17 06:52:24 -0500

    Removed unnecessary folder and updated project structure

--- djangotest/djangotest/tracker/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Activity
-
-@admin.register(Activity)
-class ActivityAdmin(admin.ModelAdmin):
-    list_display = ('user', 'name', 'duration_minutes', 'heart_rate', 'calories_burned', 'date')
-    list_filter = ('user', 'name', 'date')
-    search_fields = ('user__username', 'name')
-    ordering = ('-date',)
